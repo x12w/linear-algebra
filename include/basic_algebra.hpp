6b432df4cfbd713676c3e1254f07d73b1733b45a
x12w <w17802627260@gmail.com>
2026-05-11 22:43:18 +0800

    Signed-off-by: x12w <w17802627260@gmail.com>
    feat:实现RationalNumber类

--- include/basic_algebra.hpp
+++ include/basic_algebra.hpp
@@ -1,6 +1,11 @@
+#include <cmath>
+#include <csignal>
 #include <iostream>
 #include <istream>
+#include <iterator>
+#include <memory>
 #include <ostream>
+#include <stdexcept>
 
 #ifndef BASIC_ALGEBRA_HPP
 #define BASIC_ALGEBRA_HPP
@@ -308,6 +313,171 @@ std::istream &operator>>(std::istream &is, ComplexNumber<T> &operand) {
   return is;
 }
 
+// to define the rational number type, you should provide a integer type,
+// the integer type should imlement the following operators
+// +, -, *, /, %, +=, -=, /=, *=, %=, -(the unary operator), ==, !=, >, >=, <,
+// <=, <<, >>(stream operator) the default interger type is int
+template <typename T = int> class RationalNumber {
+private:
+  using ValueType = T;
+  ValueType molecular, denominator;
+
+  // math methods to imlement the type
+  ValueType gcd(ValueType operand1, ValueType operand2) const {
+    operand1 = operand1 >= 0 ? operand1 : -operand1;
+    operand2 = operand2 >= 0 ? operand2 : -operand2;
+
+    while (operand2 != 0) {
+      ValueType temp = operand1 % operand2;
+      operand1 = operand2;
+      operand2 = temp;
+    }
+
+    return operand1;
+  }
+
+  void reduction() {
+    // check whether the molecular is 0
+    if (molecular == 0) {
+      denominator = 1;
+      return;
+    }
+
+    ValueType div = gcd(molecular, denominator);
+    molecular /= div;
+    denominator /= div;
+  }
+
+public:
+  // constructors
+  RationalNumber() : molecular(0), denominator(1) {}
+  RationalNumber(const ValueType &d, const ValueType &m = 1)
+      : molecular(d), denominator(m) {
+    // check whether the denominator is 0
+    if (m == 0) {
+      throw(std::runtime_error(
+          "error: you can't create a RationalNumber whose denominator is 0"));
+    }
+  }
+
+  // reload operators
+  RationalNumber operator+(const RationalNumber &operand) const {
+    ValueType new_molecular = molecular * operand.denominator +
+                              denominator * operand.molecular,
+              new_denominator = denominator * operand.denominator;
+    RationalNumber res(new_molecular, new_denominator);
+    res.reduction();
+    return res;
+  }
+
+  RationalNumber operator-(const RationalNumber &operand) const {
+    ValueType new_molecular = molecular * operand.denominator -
+                              denominator * operand.molecular,
+              new_denominator = denominator * operand.denominator;
+    RationalNumber res(new_molecular, new_denominator);
+    res.reduction();
+    return res;
+  }
+
+  RationalNumber operator*(const RationalNumber &operand) const {
+    RationalNumber res(molecular * operand.molecular,
+                       denominator * operand.denominator);
+    res.reduction();
+    return res;
+  }
+
+  RationalNumber operator/(const RationalNumber &operand) const {
+    RationalNumber res(molecular * operand.denominator,
+                       denominator * operand.molecular);
+    return res;
+  }
+
+  RationalNumber &operator+=(const RationalNumber &operand) {
+    *this = (*this) + operand;
+    return *this;
+  }
+
+  RationalNumber &operator-=(const RationalNumber &operand) {
+    *this = (*this) - operand;
+    return *this;
+  }
+
+  RationalNumber &operator*=(const RationalNumber &operand) {
+    *this = (*this) * operand;
+    return *this;
+  }
+
+  RationalNumber &operator/=(const RationalNumber &operand) {
+    // check whether the operand is 0
+    if (operand.molecular == 0) {
+      throw(std::runtime_error("error: a RationalNumber 0 can't be a divisor"));
+    }
+    *this = (*this) + operand;
+    return *this;
+  }
+
+  bool operator==(const RationalNumber &operand) const {
+    return ((molecular * operand.denominator) ==
+            (denominator * operand.molecular));
+  }
+
+  bool operator!=(const RationalNumber &operand) const {
+    return ((molecular * operand.denominator) !=
+            (denominator * operand.molecular));
+  }
+
+  bool operator>(const RationalNumber &operand) const {
+    return ((molecular * operand.denominator) >
+            (denominator * operand.molecular));
+  }
+
+  bool operator>=(const RationalNumber &operand) const {
+    return ((molecular * operand.denominator) >=
+            (denominator * operand.molecular));
+  }
+
+  bool operator<(const RationalNumber &operand) const {
+    return ((molecular * operand.denominator) <
+            (denominator * operand.molecular));
+  }
+
+  bool operator<=(const RationalNumber &operand) const {
+    return ((molecular * operand.denominator) <=
+            (denominator * operand.molecular));
+  }
+
+  template <typename U>
+  friend std::ostream &operator<<(std::ostream &os,
+                                  const RationalNumber<U> &operand);
+  template <typename U>
+  friend std::istream &operator>>(std::istream &is, RationalNumber<U> &operand);
+};
+
+template <typename T>
+std::ostream &operator<<(std::ostream &os, const RationalNumber<T> &operand) {
+  // check the special conditions
+  if (operand.molecular == 0) {
+    os << 0;
+    return os;
+  }
+  if (operand.denominator == 1) {
+    os << operand.molecular;
+    return os;
+  }
+  if (operand.molecular < 0) {
+    os << "-" << operand.molecular << "/" << operand.denominator;
+    return os;
+  }
+
+  os << operand.molecular << "/" << operand.denominator;
+  return os;
+}
+
+template <typename T>
+std::istream &operator>>(std::istream &is, RationalNumber<T> &operand) {
+  is >> operand.molecular >> operand.denominator;
+  return is;
+}
 } // namespace basic_algebra
 
 #endif
